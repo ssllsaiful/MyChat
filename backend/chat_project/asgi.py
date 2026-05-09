1f27175cc1d5f515156b7cbb8be5d2d1f3cd558f
Saiful Islam <ssllsaiful.islam@gmail.com>
2026-05-10 01:41:52 +0600

    fix asgi.py import order

--- backend/chat_project/asgi.py
+++ backend/chat_project/asgi.py
@@ -1,19 +1,10 @@
-"""
-ASGI config for chat_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
