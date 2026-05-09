2334c2126e9d919d87eeddc83c0131c9fc72551b
Saiful Islam <ssllsaiful.islam@gmail.com>
2026-05-10 01:44:01 +0600

    fix asgi app initialization order

--- backend/chat_project/asgi.py
+++ backend/chat_project/asgi.py
@@ -2,12 +2,14 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
