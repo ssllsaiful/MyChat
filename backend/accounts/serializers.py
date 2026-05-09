e68934a1577fdb34967dc3ca8eb1beeada70c8e8
Saiful Islam <ssllsaiful.islam@gmail.com>
2026-05-09 23:47:35 +0600

    update

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
 
@@ -10,11 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
@@ -23,24 +22,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
-
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework import exceptions
-
 class CustomTokenObtainSerializer(TokenObtainPairSerializer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['email'] = serializers.EmailField()
-
     def validate(self, attrs):
-        # The parent method validates username and password
         data = super().validate(attrs)
-        
-        # Now validate email matches the authenticated user
-        if self.user.email != attrs.get('email'):
-            raise exceptions.AuthenticationFailed(
-                'Email does not match the registered email for this username.',
-                'invalid_credentials',
-            )
-            
+        data['username'] = self.user.username
+        data['email'] = self.user.email
         return data
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, UserSerializer, CustomTokenObtainSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = UserSerializer.Meta.model.objects.all()
@@ -10,13 +11,8 @@ class RegisterView(generics.CreateAPIView):
 
 class MeView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
-
     def get(self, request):
         return Response(UserSerializer(request.user).data)
-
-
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomTokenObtainSerializer
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainSerializer
